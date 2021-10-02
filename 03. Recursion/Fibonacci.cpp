540906fe4f3d15194b3137813fd5e1f6bad2ddaa
singhankur101 <60316965+singhankur101@users.noreply.github.com>
2021-10-02 21:34:13 +0530

    Update Fibonacci.cpp

--- 03. Recursion/Fibonacci.cpp
+++ 03. Recursion/Fibonacci.cpp
@@ -4,19 +4,19 @@ using namespace std;
  
 // Function for fibonacci
  
-int fib(int n)
+int fib(int N)
 {
     // Stop condition
-    if (n == 0)
+    if (N == 0)
         return 0;
  
     // Stop condition
-    if (n == 1 || n == 2)
+    if (N == 1 || N == 2)
         return 1;
  
     // Recursion function
     else
-        return (fib(n - 1) + fib(n - 2));
+        return (fib(N - 1) + fib(N - 2));
 }
  
 // Driver Code
